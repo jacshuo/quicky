4964db4c757a43950455b20c2d9ecbeab22df2f4
jacshuo <jacshuo@gmail.com>
2022-03-23 15:33:26 +0800

    Add Redux

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,13 +3,17 @@ import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import { MemoryRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './store';
 
 render(
-  <ConfigProvider locale={zhCN}>
-    <Router>
-      <App />
-    </Router>
-  </ConfigProvider>,
+  <Provider store={store}>
+    <ConfigProvider locale={zhCN}>
+      <Router>
+        <App />
+      </Router>
+    </ConfigProvider>
+  </Provider>,
   document.getElementById('root')
 );
